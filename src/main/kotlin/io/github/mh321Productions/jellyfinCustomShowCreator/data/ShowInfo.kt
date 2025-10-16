b858d550afab10aa6abed0d5a6798fae885a0a86
Mika Herrmann <mikaherrmann1@gmx.net>
2025-10-16 10:46:37 +0200

    Add Test values to the show info

--- src/main/kotlin/io/github/mh321Productions/jellyfinCustomShowCreator/data/ShowInfo.kt
+++ src/main/kotlin/io/github/mh321Productions/jellyfinCustomShowCreator/data/ShowInfo.kt
@@ -1,11 +1,15 @@
 package io.github.mh321Productions.jellyfinCustomShowCreator.data
 
 import io.github.mh321Productions.jellyfinCustomShowCreator.data.annotations.UiIgnore
+import io.github.mh321Productions.jellyfinCustomShowCreator.data.annotations.UiUseTextArea
 import kotlinx.serialization.Serializable
 
 @Serializable
 class ShowInfo {
-    var name = ""
+    var name = "Test"
+
+    @UiUseTextArea
+    var description = "Test\nTest"
 
 
     @UiIgnore
