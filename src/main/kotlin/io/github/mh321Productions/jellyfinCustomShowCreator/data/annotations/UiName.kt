b007203e60dd770fdd0519bb2c1617ce5394e7ad
Mika Herrmann <mika.herrmann@js-soft.com>
2025-10-17 13:03:19 +0200

    Combine UI annotations

--- src/main/kotlin/io/github/mh321Productions/jellyfinCustomShowCreator/data/annotations/UiName.kt
+++
@@ -1,8 +0,0 @@
-package io.github.mh321Productions.jellyfinCustomShowCreator.data.annotations
-
-/**
- * Instruct the Ui Editor to use the given custom name
- * instead of parsing the property name
- */
-@Target(AnnotationTarget.PROPERTY)
-annotation class UiName(val name: String)
