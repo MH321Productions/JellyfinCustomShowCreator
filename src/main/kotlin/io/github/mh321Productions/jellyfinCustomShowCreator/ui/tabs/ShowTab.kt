261cb89fe508538753f7d7fc0e60116e7939c060
Mika Herrmann <mika.herrmann@js-soft.com>
2025-10-20 14:56:47 +0200

    Update border colors when theme changes

--- src/main/kotlin/io/github/mh321Productions/jellyfinCustomShowCreator/ui/tabs/ShowTab.kt
+++ src/main/kotlin/io/github/mh321Productions/jellyfinCustomShowCreator/ui/tabs/ShowTab.kt
@@ -2,22 +2,19 @@ package io.github.mh321Productions.jellyfinCustomShowCreator.ui.tabs
 
 import io.github.mh321Productions.jellyfinCustomShowCreator.data.ShowInfo
 import io.github.mh321Productions.jellyfinCustomShowCreator.ui.MainFrame
+import io.github.mh321Productions.jellyfinCustomShowCreator.ui.ThemeUtils
 import io.github.mh321Productions.jellyfinCustomShowCreator.ui.widgets.episodes.EpisodeListPanel
 import io.github.mh321Productions.jellyfinCustomShowCreator.ui.widgets.episodes.UnassignedEpisodePanel
 import io.github.mh321Productions.jellyfinCustomShowCreator.ui.widgets.metadata.MetadataPanel
 import io.github.mh321Productions.jellyfinCustomShowCreator.ui.widgets.metadata.extractors.ShowDataExtractor
 import net.miginfocom.swing.MigLayout
 import javax.swing.JScrollPane
-import javax.swing.UIManager
-import javax.swing.border.LineBorder
-import javax.swing.border.TitledBorder
 
 class ShowTab(frame: MainFrame) : Tab(frame, "Show info", null) {
 
-    private val borderColor = UIManager.getColor("Component.borderColor")
-
-    private val spMeta: JScrollPane
-    private val spEpisodes: JScrollPane
+    //Note: The scroll panes are never really null, but can be when not initialized during a call of updateUI
+    private val spMeta: JScrollPane?
+    private val spEpisodes: JScrollPane?
 
     private val panelEpisodes: EpisodeListPanel<UnassignedEpisodePanel>
     private val panelMetadata: MetadataPanel<ShowInfo>
@@ -27,12 +24,19 @@ class ShowTab(frame: MainFrame) : Tab(frame, "Show info", null) {
 
         panelEpisodes = EpisodeListPanel<UnassignedEpisodePanel>(frame)
         spEpisodes = JScrollPane(panelEpisodes)
-        spEpisodes.border = TitledBorder(LineBorder(borderColor), "Episodes", TitledBorder.LEADING, TitledBorder.TOP)
+        spEpisodes.border = ThemeUtils.createTitledBorder("Episodes")
         add(spEpisodes, "cell 0 0, grow")
 
         panelMetadata = MetadataPanel(frame, frame.show, ShowDataExtractor())
         spMeta = JScrollPane(panelMetadata)
-        spMeta.border = TitledBorder(LineBorder(borderColor), "Metadata", TitledBorder.LEADING, TitledBorder.TOP)
+        spMeta.border = ThemeUtils.createTitledBorder("Metadata")
         add(spMeta, "cell 1 0, grow")
+    }
+
+    override fun updateUI() {
+        super.updateUI()
+
+        spEpisodes?.border = ThemeUtils.createTitledBorder("Episodes")
+        spMeta?.border = ThemeUtils.createTitledBorder("Metadata")
     }
 }
