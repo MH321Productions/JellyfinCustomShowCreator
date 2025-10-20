bf5e1d22c99c25156313e8001f88fbb57fce1d18
Mika Herrmann <mika.herrmann@js-soft.com>
2025-10-20 14:56:31 +0200

    Write helper methods for borders and themed colors

--- src/main/kotlin/io/github/mh321Productions/jellyfinCustomShowCreator/ui/ThemeUtils.kt
+++ src/main/kotlin/io/github/mh321Productions/jellyfinCustomShowCreator/ui/ThemeUtils.kt
@@ -4,6 +4,10 @@ import com.formdev.flatlaf.FlatDarkLaf
 import com.formdev.flatlaf.FlatLaf
 import com.formdev.flatlaf.FlatLightLaf
 import com.formdev.flatlaf.extras.FlatAnimatedLafChange
+import java.awt.Color
+import javax.swing.UIManager
+import javax.swing.border.LineBorder
+import javax.swing.border.TitledBorder
 
 object ThemeUtils {
     fun setTheme(isDark: Boolean) {
@@ -16,4 +20,9 @@ object ThemeUtils {
         FlatLaf.updateUI()
         FlatAnimatedLafChange.hideSnapshotWithAnimation()
     }
+
+    fun getBorderColor(): Color = UIManager.getColor("Component.borderColor")
+
+    fun createLineBorder() = LineBorder(getBorderColor())
+    fun createTitledBorder(title: String) = TitledBorder(createLineBorder(), title, TitledBorder.LEADING, TitledBorder.TOP)
 }
