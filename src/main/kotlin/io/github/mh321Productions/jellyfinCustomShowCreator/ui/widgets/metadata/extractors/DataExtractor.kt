ed6e5f66e5eed69a8f5eb8a5459f887d19c88033
Mika Herrmann <mikaherrmann1@gmx.net>
2025-11-03 00:34:57 +0100

    Add the possibility for custom spinner formatters

--- src/main/kotlin/io/github/mh321Productions/jellyfinCustomShowCreator/ui/widgets/metadata/extractors/DataExtractor.kt
+++ src/main/kotlin/io/github/mh321Productions/jellyfinCustomShowCreator/ui/widgets/metadata/extractors/DataExtractor.kt
@@ -3,6 +3,9 @@ package io.github.mh321Productions.jellyfinCustomShowCreator.ui.widgets.metadata
 import io.github.mh321Productions.jellyfinCustomShowCreator.ui.MainFrame
 import io.github.mh321Productions.jellyfinCustomShowCreator.ui.widgets.metadata.PropertyInfo
 import io.github.mh321Productions.jellyfinCustomShowCreator.ui.widgets.metadata.UpdatedDocumentListener
+import io.github.mh321Productions.jellyfinCustomShowCreator.util.get
+import io.github.mh321Productions.jellyfinCustomShowCreator.util.toDate
+import java.util.Date
 import javax.swing.*
 
 abstract class DataExtractor<T>(protected val frame: MainFrame) {
@@ -20,16 +23,30 @@ abstract class DataExtractor<T>(protected val frame: MainFrame) {
         return PropertyInfo(name, comp)
     }
 
-    protected fun numberSpinner(name: String, initialValue: Number, min: Number, max: Number, step: Number, updateListener: (Number) -> Unit)
-        = spinner(name, SpinnerNumberModel(initialValue, max as Comparable<*>, min as Comparable<*>, step), updateListener)
+    protected fun numberSpinner(name: String, initialValue: Number, min: Number, max: Number, step: Number, decimalFormat: String, updateListener: (Number) -> Unit)
+        = spinner(
+            name,
+            SpinnerNumberModel(initialValue, min as Comparable<*>, max as Comparable<*>, step),
+            { JSpinner.NumberEditor(it, decimalFormat) },
+            updateListener
+        )
+
+    protected fun dateSpinner(name: String, initialValue: Int, min: Int, max: Int, calendarField: Int, dateFormat: String, updateListener: (Int) -> Unit)
+        = spinner(
+            name,
+            SpinnerDateModel(initialValue.toDate(calendarField), min.toDate(calendarField), max.toDate(calendarField), calendarField),
+            { JSpinner.DateEditor(it, dateFormat) },
+            dateUpdateListener(calendarField, updateListener)
+        )
 
     @Suppress("UNCHECKED_CAST")
-    protected fun <T> spinner(name: String, model: SpinnerModel, updateListener: (T) -> Unit): PropertyInfo {
+    protected fun <T> spinner(name: String, model: SpinnerModel, editor: (JSpinner) -> JComponent, updateListener: (T) -> Unit): PropertyInfo {
         val comp = JSpinner(model)
         comp.addChangeListener {
             updateListener(comp.value as T)
             frame.markDirty()
         }
+        comp.editor = editor(comp)
         return PropertyInfo(name, comp)
     }
 
@@ -44,4 +61,6 @@ abstract class DataExtractor<T>(protected val frame: MainFrame) {
         }
         return PropertyInfo(name, comp)
     }
+
+    private fun dateUpdateListener(calendarField: Int, updateListener: (Int) -> Unit): (Date) -> Unit = { updateListener(it[calendarField]) }
 }
