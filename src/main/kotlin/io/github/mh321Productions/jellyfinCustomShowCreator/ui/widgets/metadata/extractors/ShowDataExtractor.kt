03c5e44bf91f0c6454b80c4cb78ed7ed5cabcacf
Mika Herrmann <mikaherrmann1@gmx.net>
2025-11-03 00:41:36 +0100

    Add a date spinner for the release year of the show

--- src/main/kotlin/io/github/mh321Productions/jellyfinCustomShowCreator/ui/widgets/metadata/extractors/ShowDataExtractor.kt
+++ src/main/kotlin/io/github/mh321Productions/jellyfinCustomShowCreator/ui/widgets/metadata/extractors/ShowDataExtractor.kt
@@ -2,11 +2,13 @@ package io.github.mh321Productions.jellyfinCustomShowCreator.ui.widgets.metadata
 
 import io.github.mh321Productions.jellyfinCustomShowCreator.data.ShowInfo
 import io.github.mh321Productions.jellyfinCustomShowCreator.ui.MainFrame
+import java.util.Calendar
 
 class ShowDataExtractor(frame: MainFrame) : DataExtractor<ShowInfo>(frame) {
     override fun getWidgets(data: ShowInfo) = listOf(
         textField("Show Name: ", data.name) { data.name = it },
         textArea("Show Description: ", data.description) { data.description = it },
         comboBox("Show Status: ", data.status) { data.status = it },
+        dateSpinner("Year: ", data.year, 1800, 2100, Calendar.YEAR, "yyyy") { data.year = it },
     )
 }
