79da79b7bbbe890841fab1f6a3ee49572bb48ca0
Mika Herrmann <mikaherrmann1@gmx.net>
2025-11-03 00:34:12 +0100

    Add filesystem watch and coroutines dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,10 +18,13 @@ dependencies {
 
     implementation("io.github.pdvrieze.xmlutil:core:0.91.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+    implementation("io.github.irgaly.kfswatch:kfswatch:1.4.0")
 
     implementation("com.miglayout:miglayout-swing:11.4.2")
     implementation("com.formdev:flatlaf:3.6.1")
     implementation("com.formdev:flatlaf-extras:3.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.10.2")
 
     implementation("com.github.Dansoftowner:jSystemThemeDetector:3.9.1")
     implementation("org.slf4j:slf4j-jdk14:2.0.17")
